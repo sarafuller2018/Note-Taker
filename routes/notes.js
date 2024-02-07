fee5f8c621642a2e5718c203ec4e587292df9f7d
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-02-07 13:37:57 -0600

    working on bugs for delete method

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,16 @@
 const notes = require("express").Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uniqid = require("uniqid");
+const fs = require("fs");
 
 // GET request
 notes.get("/", (req, res) => {
-    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // POST request
 notes.post("/", (req, res) => {
-const { title, text } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
@@ -27,22 +28,29 @@ const { title, text } = req.body;
 
 // DELETE request
 notes.delete("/:id", (req, res) => {
-  
+
   const id = req.params.id;
 
   if (id) {
     console.log(`Deleting note with id: ${id}`)
-    
+
     function checkID(db) {
-     return db !== id }
+      console.log("db is: ", db);
+      return db.id !== id
+    }
 
     readFromFile("./db/db.json")
-    .then((data) => { 
-      const parsedData = JSON.parse(data);
-      const filteredData = parsedData.filter(checkID);
-      console.log(filteredData);
-      return filteredData;
-    })
-  }});
+      .then((data) => {
+        const parsedData = JSON.parse(data);
+        const filteredData = parsedData.filter(checkID);
+        console.log(filteredData);
+
+        fs.writeFile('./db/db.json', JSON.stringify(filteredData) || [], (err) =>
+          err ? console.error(err) : console.log('Success!'));
+      });
+  }
+
+  reload();
+});
 
 module.exports = notes;
