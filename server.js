176bfb6530552a7cc9f643f669cb4558fbeae0dd
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-02-07 15:19:19 -0600

    added wildcard route

--- server.js
+++ server.js
@@ -22,4 +22,9 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+// GET request route for homepage for any route that does not exist
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
 app.listen(PORT, () => { console.log(`App listening at http://localhost:${PORT}`) }) // connects to port and runs function, must start server.js in terminal for port to be accessed on localhost3001
